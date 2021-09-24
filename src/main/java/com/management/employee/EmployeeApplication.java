26fa1ef3985473cc3b6722fe7ad213429b279f94
Sirsendu Gope <sirsendugope1994@gmail.com>
2021-09-25 00:33:48 +0530

    Configure cros

--- src/main/java/com/management/employee/EmployeeApplication.java
+++ src/main/java/com/management/employee/EmployeeApplication.java
@@ -3,7 +3,10 @@ package com.management.employee;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
@@ -16,5 +19,14 @@ public class EmployeeApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(EmployeeApplication.class, args);
 	}
-
+	
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/api/employee/list").allowedOrigins("http://localhost:3000");
+			}
+		};
+	}
 }
